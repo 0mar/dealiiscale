a882da16ed0989f0600bf9bbf5c3bd382b055baa
Omar Richardson <omsrichardson@gmail.com>
2019-06-28 20:43:15 +0200

    Implemented flux integral in right hand side

--- src/elliptic-parabolic/pi_solver.cpp
+++ src/elliptic-parabolic/pi_solver.cpp
@@ -174,11 +174,12 @@ void PiSolver<dim>::set_micro_solutions(std::vector<Vector<double>> *_solutions,
 }
 
 template<int dim>
-double PiSolver<dim>::get_micro_mass(unsigned int micro_index) {
-    // computed as: f(x) = \int_Y \rho(x,y)dy
+double PiSolver<dim>::get_micro_mass(unsigned int micro_index) { // todo: Make const
+    // computed as: f(x) = \int_Y \rho(x,y) dy
     double integral = 0;
     QGauss<dim> quadrature_formula(integration_order);
-    FEValues<dim> fe_values(fe, quadrature_formula, update_values | update_quadrature_points | update_JxW_values);
+    FEValues<dim> fe_values(micro_dof_handler->get_fe(), quadrature_formula,
+                            update_values | update_quadrature_points | update_JxW_values);
     const unsigned int dofs_per_cell = fe.dofs_per_cell;
     const unsigned int n_q_points = quadrature_formula.size();
     std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
@@ -195,8 +196,30 @@ double PiSolver<dim>::get_micro_mass(unsigned int micro_index) {
 }
 
 template<int dim>
-double PiSolver<dim>::get_micro_flux(unsigned int_micro_index) {
-    return 0;
+double PiSolver<dim>::get_micro_flux(unsigned int micro_index) {
+    const int ROBIN_BOUNDARY = 0; //Not linked, a bit ugly
+    // Computed as: f(x) = \int_\Gamma_R \nabla_y \rho(x,y) \cdot n_y d_\sigma_y
+    double integral = 0;
+    QGauss<dim - 1> quadrature_formula(integration_order);
+    FEFaceValues<dim> fe_face_values(micro_dof_handler->get_fe(),
+                                     quadrature_formula, // Todo: Check flags (update_values), also in other places.
+                                     update_values | update_quadrature_points | update_JxW_values |
+                                     update_normal_vectors | update_gradients);
+    const unsigned int n_q_face_points = quadrature_formula.size();
+    for (const auto &cell: micro_dof_handler->active_cell_iterators()) {
+        for (unsigned int face_number = 0; face_number < GeometryInfo<dim>::faces_per_cell; face_number++) {
+            if (cell->face(face_number)->at_boundary() && cell->face(face_number)->boundary_id() == ROBIN_BOUNDARY) {
+                fe_face_values.reinit(cell, face_number);
+                std::vector<Tensor<1, dim>> solution_gradient(n_q_face_points);
+                fe_face_values.get_function_gradients(micro_solutions->at(micro_index), solution_gradient);
+                for (unsigned int q_index = 0; q_index < n_q_face_points; q_index++) {
+                    double neumann = solution_gradient[q_index] * fe_face_values.normal_vector(q_index);
+                    integral += neumann * fe_face_values.JxW(q_index);
+                }
+            }
+        }
+    }
+    return integral;
 }
 
 template<int dim>
