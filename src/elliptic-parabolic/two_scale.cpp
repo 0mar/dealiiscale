9b745871f4b24a6cdb43411f25a6faf966ae5ce6
Omar Richardson <omsrichardson@gmail.com>
2019-07-13 12:55:00 +0200

    Optional refinement parameter

--- src/elliptic-parabolic/two_scale.cpp
+++ src/elliptic-parabolic/two_scale.cpp
@@ -10,13 +10,21 @@
  * Run a multiscale elliptic-parabolic solver
  * @return 0
  */
-int main() {
+int main(int argc, char *argv[]) {
+    int micro_refine = 3;
+    int macro_refine = 3;
+    if (argc >= 2) {
+        macro_refine = std::stoi(argv[1]);
+    }
+    if (argc >= 3) {
+        micro_refine = std::stoi(argv[2]);
+    }
     dealii::deallog.depth_console(0);
     std::string file_name = "two-scale-convergence.txt";
     std::ofstream ofs;
     ofs.open("results/" + file_name, std::ofstream::out | std::ofstream::trunc);
     ofs.close();
-    TimeManager manager(3, 3);
+    TimeManager manager(macro_refine, micro_refine);
     manager.set_ct_file_name(file_name);
     manager.setup();
     manager.run();
