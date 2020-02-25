86ce680d890c8df9a59e02b8f6fcf7d77d72c369
Omar Richardson <omar.online@mailbox.org>
2020-02-25 14:24:47 +0000

    Another debugging ocmmit

--- src/playground/transform.cpp
+++ src/playground/transform.cpp
@@ -144,6 +144,7 @@ void NonLinDomainMapping<dim>::get_kkt(const Point<dim> &p, SymmetricTensor<2, d
     Tensor<2, dim> jacobian = solution_base.map_jac->value(p);
     Tensor<2, dim> inv_jacobian = invert(jacobian);
     det_jac = determinant(jacobian);
+    std::cout << "Jac " << jacobian << "\ninv_jac " << inv_jacobian << "\ndet " << det_jac << std::endl;
     kkt = SymmetricTensor<2, dim>(inv_jacobian * transpose(inv_jacobian));
 }
 
@@ -251,6 +252,7 @@ void RobinSolver::assemble_system() {
         for (unsigned int q_index = 0; q_index < n_q_points; ++q_index) {
             double det_jac;
             dm.get_kkt(fe_values.quadrature_point(q_index), kkt, det_jac);
+            std::cout << kkt << std::endl;
             for (unsigned int i = 0; i < dofs_per_cell; ++i) {
                 for (unsigned int j = 0; j < dofs_per_cell; ++j) {
                     cell_matrix(i, j) += (fe_values.shape_grad(i, q_index) *
@@ -262,7 +264,9 @@ void RobinSolver::assemble_system() {
                                 solution_base.rhs.value(dm.map(fe_values.quadrature_point(q_index))) *
                                 det_jac *
                                 fe_values.JxW(q_index));
+                std::cout << "rhs " << cell_rhs(i) << std::endl;
             }
+            exit(0);
         }
         cell->get_dof_indices(local_dof_indices);
         for (unsigned int i = 0; i < dofs_per_cell; ++i) {
