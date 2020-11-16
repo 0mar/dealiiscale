3444985a4525e793472c4689f5dd7ded19551a4a
0mar <omar.richardson@kau.se>
2020-11-16 10:07:45 +0100

    Updated executable with arguments

--- src/bio-multiscale/manufactured.cpp
+++ src/bio-multiscale/manufactured.cpp
@@ -21,36 +21,51 @@ std::string get_id(const std::string &path) {
     return id;
 }
 
-void run(const std::string &input_path, int num_threads) {
+void conv_test(const std::string &input_path, int macro_refinement, int micro_refinement, int num_threads) {
     const std::string id = get_id(input_path);
     const std::string output_path = "results/" + id + "_" + "convergence_table.txt";
     std::ofstream ofs;
     ofs.open(output_path, std::ofstream::out | std::ofstream::trunc);
     ofs.close();
     for (unsigned int i = 4; i < 5; i++) {
-        Manager manager(i - 1, i, input_path, output_path, num_threads);
+        Manager manager(macro_refinement, micro_refinement, input_path, output_path, num_threads);
         manager.setup();
         manager.run();
     }
+}
+void run(const std::string &input_path, int macro_refinement, int micro_refinement, int num_threads) {
+    const std::string output_path = "/dev/null";
+    Manager manager(macro_refinement, micro_refinement, input_path, output_path, num_threads);
+    manager.setup();
+    manager.run();
 }
 
 int main(int argc, char *argv[]) {
     dealii::deallog.depth_console(0);
     std::string input_path = "input/linear.prm";
+    int macro_refinement = 3;
+    int micro_refinement = 3;
     int num_threads = 0;
     if (argc >= 2) {
         input_path = argv[1];
     }
     if (argc >= 3) {
-        num_threads = std::stoi(argv[2]);
-    } else if (argc > 3) {
-        std::cout << "Too many arguments" << std::endl;
+        macro_refinement = std::stoi(argv[2]);
+    }
+    if (argc >= 4) {
+        micro_refinement = std::stoi(argv[3]);
+    }
+    if (argc >= 5) {
+        num_threads = std::stoi(argv[4]);
+    } else if (argc > 5) {
+        std::cout << "Too many arguments" << std::endl << "Supply (1) input file, (2) macro refinement, (3) micro refinement, (4) number of cores" << std::endl;
         return 1;
     }
     dealii::Timer timer;
     timer.start();
-    run(input_path, num_threads);
+    run(input_path, macro_refinement, micro_refinement, num_threads);
     timer.stop();
+    printf("Parameters: macro %d, micro %d, %d threads\n", macro_refinement, micro_refinement, num_threads);
     printf("Ran in %.2f seconds with %.2f CPU time\n", timer.wall_time(), timer.cpu_time());
     if (num_threads != 0) {
         const std::string id = get_id(input_path);
